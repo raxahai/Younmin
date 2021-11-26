7e7a8668f0113e6a70c16e770d048fe21f178bfa
Raza Haider <raxa.hai@gmail.com>
2021-11-26 20:15:57 +0500

    feat(validator): Add isRequired validator

--- lib/globals/validators.dart
+++ lib/globals/validators.dart
@@ -2,9 +2,13 @@ import 'package:email_validator/email_validator.dart';
 
 class Validators {
   static String? isValidEmail(String? value) {
-    if (value == null) return "email is required to subscribe";
+    if (value!.isEmpty) return "email is required";
     if (!EmailValidator.validate(value)) {
       return "please enter a valid email";
     }
+  }
+
+  static String? isRequired(String? value) {
+    if (value!.isEmpty) return "Required";
   }
 }
