b77bcf1e1d9d2291d2ea3126a46b1e8b312d3ba9
Raza Haider <raxa.hai@gmail.com>
2021-11-27 19:47:13 +0500

    feat(Validators): Add signUp field validator

--- lib/globals/validators.dart
+++ lib/globals/validators.dart
@@ -11,4 +11,45 @@ class Validators {
   static String? isRequired(String? value) {
     if (value!.isEmpty) return "Required";
   }
+
+  static String? isValidFirstName(String? value) {
+    if (value!.length < 3) {
+      return "First name must be at least 3 characters";
+    }
+    return null;
+  }
+
+  static String? isValidLastName(String? value) {
+    if (value!.length < 3) {
+      return "Last name must be at least 3 characters";
+    }
+    return null;
+  }
+
+  static String? isValidAge(String? value) {
+    if (value!.isEmpty) {
+      return "required";
+    }
+    if (double.parse(value) >= 13 && double.parse(value) <= 150) {
+      return null;
+    }
+    return "between 8 & 150";
+  }
+
+  static String? isValidPassword(String? value) {
+    if (value!.length < 6) {
+      return "Must be at least 6 characters";
+    }
+    return null;
+  }
+
+  static String? isValidConfirmPassword(
+      String? value, String? passwordToCompare) {
+    String? message = isValidPassword(value);
+    if (passwordToCompare!.isEmpty) return message;
+    if (message == null && value != passwordToCompare) {
+      message = 'Same password required';
+    }
+    return message;
+  }
 }
