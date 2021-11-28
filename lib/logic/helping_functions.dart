a30761b56157ff12444f67294c69ea5c7e30de57
Raza Haider <raxa.hai@gmail.com>
2021-11-28 16:05:36 +0500

    Refactor(code): Restructured firebaseHandler

--- lib/logic/helping_functions.dart
+++ lib/logic/helping_functions.dart
@@ -83,40 +83,6 @@ Future<int?> facebookCalculateUserAge(authData) async {
   return null;
 }
 
-bool handleFirebaseError(err, context) {
-  if (err
-      .toString()
-      .contains("[firebase_auth/account-exists-with-different-credential]")) {
-    _errorSnackBar(context,
-        message:
-            "this account contains email that already in use, try another account");
-
-    return true;
-  } else if (err.toString().contains("[firebase_auth/email-already-in-use]")) {
-    _errorSnackBar(context,
-        message: "this email already in use, try another one");
-    return true;
-  } else if (err.toString().contains("[firebase_auth/user-not-found]") ||
-      err.toString().contains("[firebase_auth/wrong-password]")) {
-    _errorSnackBar(context, message: "password or email is wrong");
-    return true;
-  } else if (err != null) {
-    _errorSnackBar(context, message: "something went wrong, try again later");
-    return true;
-  }
-  return false;
-}
-
-void _errorSnackBar(context, {message}) {
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      content: Text(message),
-      backgroundColor: Colors.red,
-      duration: const Duration(milliseconds: 1500),
-    ),
-  );
-}
-
 Future<DocumentReference?> addDataIfFirstTime({isMale, age}) async {
   final docs = await db
       .collection("users")
