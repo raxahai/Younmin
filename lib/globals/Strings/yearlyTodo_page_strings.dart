e86af91c6086e663e1e9d50fce54ad4674438de0
Raza Haider <raxa.hai@gmail.com>
2021-12-05 18:32:22 +0500

    perf(strings): Add page strings

---
+++ lib/globals/Strings/yearlyTodo_page_strings.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: file_names
+
+class YearlyTodoStrings {
+  static const String feelingAboutTask = "feeling about this task: ";
+  static const String showDailyTask = "show daily tasks";
+  static const String tasks = "Tasks: ";
+  static const String welcomeBack = "Welcome back, how are you feeling today";
+  static const String addCheck = "Add check";
+  static const String editProfilePicture = "Edit profile picture";
+  static const String deleteTask = "Delete this task?";
+  static const String close = "Close";
+  static const String delete = "Delete";
+  static const String editYourGoal = "Edit your goal";
+  static const String whatToDoYear = "What do you wanna do for this year?";
+  static const String feelingAchieveGoal =
+      "That feeling when you achieve your goal";
+  static const String editGoal = "Edit goal";
+}
