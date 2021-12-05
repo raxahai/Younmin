6929edb23421e74dd2d9505d5c8ca1a25dcc99f6
Raza Haider <raxa.hai@gmail.com>
2021-12-06 01:07:14 +0500

    feat(string): Add subscribe string

--- lib/globals/Strings/global_strings.dart
+++ lib/globals/Strings/global_strings.dart
@@ -3,6 +3,7 @@ class GlobalStrings {
   static String signUp = "Sign up";
   static String login = "Login";
   static String learnMore = "Learn more";
+  static String subscribe = "Subscribe";
   static const String enterEmail = "Enter your email address";
   static const String sendEmail = "Send emails";
 }
