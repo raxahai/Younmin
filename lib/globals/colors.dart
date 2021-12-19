ea0ca213f642b74526070308fde72c47ca8e89fc
Raza Haider <raxa.hai@gmail.com>
2021-12-19 22:49:38 +0500

    Refactor(code): removed unnecessary comments

--- lib/globals/colors.dart
+++ lib/globals/colors.dart
@@ -28,24 +28,3 @@ class YounminColors {
   static MaterialStateProperty<Color> materialStatePrimaryColor =
       MaterialStateProperty.all<Color>(primaryColor);
 }
-
-// class YounminColors {
-//   static const Color primaryColor = Color(0xff14a693);
-//   static const Color darkPrimaryColor = Color(0xff1F6C66);
-//   static const Color faceBookColor = Color(0xff5F72D9);
-//   static const Color googleColor = Color(0xffdb4a39);
-//   static const Color primaryButtonColor = Color(0xffffe926);
-//   static const Color textHeadline1Color = Color(0xff3e3e3e);
-//   static const Color backGroundColor = Color(0xffedfdf6);
-//   static const Color checkBoxColor = Color(0xff3cc80b);
-//   static const Color yearlyTodoListColor = Color(0xfff5f6fa);
-//   static const Color textFieldColor = Color(0xff666666);
-//   static const Color badSentenceColor = Color(0xffce4554);
-//   static const Color textFieldErrorColor = Colors.red;
-//   static const Color redColor = Colors.red;
-//   static const Color lightGreyColor = Color(0xffD3D3D3);
-//   static const Color transparentColor = Colors.transparent;
-//   static const Color whiteSmokeColor = Color(0xffF5F5F5);
-//   static MaterialStateProperty<Color> materialStatePrimaryColor =
-//       MaterialStateProperty.all<Color>(primaryColor);
-// }
